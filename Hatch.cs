f707c7413d83521e5a6a1ac8abc15a439c7ed593
maretzky <marek.siuda711@outlook.com>
2024-08-06 15:36:14 +0200

    Createn method to get list of sensor statuses

--- Hatch.cs
+++ Hatch.cs
@@ -1,96 +1,79 @@
 using System.Device.Gpio;
 using System.Threading;
 
-namespace console1 {
-    public abstract class Hatch : IHatch {
+namespace console1
+{
+    public abstract class Hatch : IHatch
+    {
         private int _upperSensorPin;
         private int _lowerSensorPin;
         private int _middleSensorPin;
-        private int _bridgeEnablePin;
-        private int _CWPolarisationPin;
-        private int _CCwPolarisationPin;
-
-        public int UpperSensorPin {
+        protected int _bridgeEnablePin;
+        public int UpperSensorPin
+        {
             get { return _upperSensorPin; }
             set { _upperSensorPin = value; }
         }
-
-        public int LowerSensorPin {
+        public int LowerSensorPin
+        {
             get { return _lowerSensorPin; }
             set { _lowerSensorPin = value; }
         }
-
-        public int MiddleSensorPin {
+        public int MiddleSensorPin
+        {
             get { return _middleSensorPin; }
             set { _middleSensorPin = value; }
         }
 
-        public int BridgeEnablePin {
+        public int BridgeEnablePin
+        {
             get { return _bridgeEnablePin; }
             set { _bridgeEnablePin = value; }
         }
 
-        public int CWPolarisationPin {
-            get { return _CWPolarisationPin; }
-            set { _CWPolarisationPin = value; }
-        }
-
-        public int CCwPolarisationPin {
-            get { return _CCwPolarisationPin; }
-            set { _CCwPolarisationPin = value; }
-        }
-
-        public virtual void OpenOutPins(GpioController gpio) {
-            gpio.OpenPin(this.BridgeEnablePin, PinMode.Output);
-            gpio.OpenPin(this.CWPolarisationPin, PinMode.Output);
-            gpio.OpenPin(this.CCwPolarisationPin, PinMode.Output);
-        }
-
-        public virtual void CloseOutPins(GpioController gpio) {
-            gpio.ClosePin(this.BridgeEnablePin);
-            gpio.ClosePin(this.CWPolarisationPin);
-            gpio.ClosePin(this.CCwPolarisationPin);
-        }
-
-        public virtual void OpenHatch(GpioController gpio, double steepLevel) {
-            RotorCtrl rotor = new RotorCtrl();
-            gpio.OpenPin(23, PinMode.Output);
-            gpio.Write(23, PinValue.High);
-
-            rotor.StartClockwise(steepLevel);
-
-            gpio.Write(23, PinValue.Low);
-            gpio.ClosePin(23);
-        }
-        public virtual void CloseHatch(GpioController gpio, double steepLevel) {
-            RotorCtrl rotor = new RotorCtrl();
-            gpio.OpenPin(23, PinMode.Output);
-            gpio.Write(23, PinValue.High);
-
-            rotor.StartCounterClockwise(steepLevel);
-
-            gpio.Write(23, PinValue.Low);
-            gpio.ClosePin(23);
-        }
-
-        public virtual PinValue GetUpperStatus(GpioController gpio) {
-            Console.WriteLine("Stan górnego czujnika: " + gpio.Read(this.UpperSensorPin));
-            return gpio.Read(this.UpperSensorPin); 
-        }
-
-        public virtual PinValue GetMiddleStatus(GpioController gpio) {
-            Console.WriteLine("Stan dolnego czujnika " + gpio.Read(this.MiddleSensorPin));
-            return gpio.Read(this.MiddleSensorPin); 
-        }
-
-        public virtual bool GetLowerStatus(GpioController gpio) {
-            // Console.WriteLine("Stan dolnego czujnika " + (bool)gpio.Read(this.LowerSensorPin));
-            // return (bool)gpio.Read(this.LowerSensorPin); 
-            return true;
-        }
-
-        public virtual void Process(GpioController gpio) {
+        public virtual void OpenHatch(GpioController gpio)
+        {
             throw new NotImplementedException();
+        }
+
+        public virtual void CloseHatch(GpioController gpio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Process(GpioController gpio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SenInit(GpioController gpio)
+        {
+            gpio.OpenPin(_upperSensorPin, PinMode.InputPullUp);
+            gpio.OpenPin(_lowerSensorPin, PinMode.InputPullUp);
+            gpio.OpenPin(_middleSensorPin, PinMode.InputPullUp);
+        }
+
+        public void SenDeInit(GpioController gpio)
+        {
+            gpio.ClosePin(_upperSensorPin);
+            gpio.ClosePin(_lowerSensorPin);
+            gpio.ClosePin(_middleSensorPin);
+        }
+
+        public List<PinValue> GetSenStat(GpioController gpio)
+        {
+            var statuses = new List<PinValue>();
+
+            PinValue upperStatus = gpio.Read(_upperSensorPin);
+            statuses.Add(upperStatus);
+
+            PinValue lowerStatus = gpio.Read(_lowerSensorPin);
+            statuses.Add(lowerStatus);
+
+            PinValue middleStatus = gpio.Read(_middleSensorPin);
+            statuses.Add(middleStatus);
+
+            return statuses;
         }
     }
 }
