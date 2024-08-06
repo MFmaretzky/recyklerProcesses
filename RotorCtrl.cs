6d3f748983dd8ce2132a66866223239902241eb9
maretzky <marek.siuda711@outlook.com>
2024-08-06 15:34:43 +0200

    Deleted disposing and initialising from process methods

--- RotorCtrl.cs
+++ RotorCtrl.cs
@@ -1,60 +1,55 @@
 using System.Device.Gpio;
 using System.Device.Pwm;
 
-namespace console1 {
-    class RotorCtrl {
-        private double dutyCycle = 0.01;
-        public double DutyCycle {
-            get { return dutyCycle; }
-            set { dutyCycle = value; }
+namespace console1
+{
+    class RotorCtrl(
+        int chip,
+        int channel,
+        int frequency,
+        double dutyCycle,
+        double steepLevel,
+        double maxSpeed
+    )
+    {
+        private readonly int _chip = chip;
+        private readonly int _channel = channel;
+        private readonly int _frequency = frequency;
+        private readonly double _dutyCycle = dutyCycle;
+        private readonly double _steepLevel = steepLevel;
+        private readonly double _maxSpeed = maxSpeed;
+        public PwmChannel Init()
+        {
+            var pwmPin = PwmChannel.Create(_chip, _channel, _frequency, _steepLevel);
+            pwmPin.Start();
+            return pwmPin;
         }
-        public void StartClockwise(double steepLevel) {
-            var pwmPin18 = PwmChannel.Create(2, 2, 400, 0.0); // chip2 channel2 - pin18
-            pwmPin18.Start();
-            pwmPin18.DutyCycle = 0;
-
-            while (pwmPin18.DutyCycle < 0.95) {
-                pwmPin18.DutyCycle += steepLevel;
-
-                Thread.Sleep(10);  
+        public void StartClockwise(PwmChannel pwmPin)
+        {
+            // chip2 channel2 - pin18
+            while (pwmPin.DutyCycle < 0.95)
+            {
+                pwmPin.DutyCycle += _steepLevel;
+                Thread.Sleep(10);
             }
-            if (pwmPin18.DutyCycle >= 0.95) { 
-                Thread.Sleep(1000);
-            } 
-            
-            while (pwmPin18.DutyCycle > 0.05) {
-                pwmPin18.DutyCycle -= steepLevel;
-                
-                Thread.Sleep(10);  
-            }
-            
-            pwmPin18.DutyCycle = 0;
-            pwmPin18.Stop();
-            pwmPin18.Dispose();
         }
-        public void StartCounterClockwise(double steepLevel) {
-            var pwmPin19 = PwmChannel.Create(2, 3, 400, 0.0); // chip2 channel3 - pin19
-            pwmPin19.Start();
-            pwmPin19.DutyCycle = 0;
 
-            while (pwmPin19.DutyCycle < 0.95) {
-                pwmPin19.DutyCycle += steepLevel;
+        public void StartCounterClockwise(PwmChannel pwmPin)
+        {
+            // chip2 channel3 - pin19
+            pwmPin.Start();
 
-                Thread.Sleep(10);  
+            while (pwmPin.DutyCycle < 0.95)
+            {
+                pwmPin.DutyCycle += _steepLevel;
+                Thread.Sleep(10);
             }
-            if (pwmPin19.DutyCycle >= 0.95) {
-                Thread.Sleep(1000);
-            }
-            
-            while (pwmPin19.DutyCycle > 0.05) {
-                pwmPin19.DutyCycle -= steepLevel;
-                
-                Thread.Sleep(10);  
-            }
-
-            pwmPin19.DutyCycle = 0;
-            pwmPin19.Stop();
-            pwmPin19.Dispose();
+        }
+        public void Stop(PwmChannel pwmPin)
+        {
+            pwmPin.DutyCycle = 0;
+            pwmPin.Stop();
+            pwmPin.Dispose();
         }
     }
 }
