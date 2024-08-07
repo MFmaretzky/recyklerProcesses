4d14ed7a3311c9fe4e2034a2742dd743ed41932f
maretzky <marek.siuda711@outlook.com>
2024-08-07 11:40:21 +0200

    It finally works as supposed to

--- FrontHatch.cs
+++ FrontHatch.cs
@@ -6,24 +6,36 @@ namespace console1
 {
     public sealed class FrontHatch : Hatch
     {
-        public FrontHatch()
+        public FrontHatch(int bridgeEnablePin, int upperSensorPin, int lowerSensorPin, int middleSensorPin)
         {
-            BridgeEnablePin = 23;
+            BridgeEnablePin = bridgeEnablePin;
+            UpperSensorPin = upperSensorPin;
+            LowerSensorPin = lowerSensorPin;
+            MiddleSensorPin = middleSensorPin;
         }
 
         public override void OpenHatch(GpioController gpio)
         {
+            gpio.OpenPin(_bridgeEnablePin, PinMode.Output);
             RotorCtrl rotor = new(2, 2, 400, 0.0, 0.05, 0.95);
             var pwmPin = rotor.Init();
-            gpio.OpenPin(_bridgeEnablePin, PinMode.Output);
-            gpio.Write(_bridgeEnablePin, PinValue.High);
 
-            rotor.StartClockwise(pwmPin);
-            do
+            List<PinValue> statuses = GetSenStat(gpio);
+
+            if (statuses[1] == PinValue.Low)
             {
-                ;
-            } while (false);
-            rotor.Stop(pwmPin);
+                gpio.Write(_bridgeEnablePin, PinValue.High);
+                rotor.StartClockwise(pwmPin);
+                do
+                {
+                    statuses = GetSenStat(gpio);
+                } while (statuses[0] == PinValue.High || statuses[2] == PinValue.High);
+                rotor.Stop(pwmPin);
+            }
+            else
+            {
+                Console.WriteLine("Error, exiting now.");
+            }
 
             gpio.Write(_bridgeEnablePin, PinValue.Low);
             gpio.ClosePin(_bridgeEnablePin);
@@ -31,17 +43,26 @@ namespace console1
 
         public override void CloseHatch(GpioController gpio)
         {
+            gpio.OpenPin(_bridgeEnablePin, PinMode.Output);
             RotorCtrl rotor = new(2, 3, 400, 0.0, 0.05, 0.95);
             var pwmPin = rotor.Init();
-            gpio.OpenPin(_bridgeEnablePin, PinMode.Output);
-            gpio.Write(_bridgeEnablePin, PinValue.High);
 
-            rotor.StartCounterClockwise(pwmPin);
-            do
+            List<PinValue> statuses = GetSenStat(gpio);
+
+            if (statuses[0] == PinValue.Low)
             {
-                ;
-            } while (false);
-            rotor.Stop(pwmPin);
+                gpio.Write(_bridgeEnablePin, PinValue.High);
+                rotor.StartCounterClockwise(pwmPin);
+                do
+                {
+                    statuses = GetSenStat(gpio);
+                } while (statuses[1] == PinValue.High);
+                rotor.Stop(pwmPin);
+            }
+            else
+            {
+                Console.WriteLine("Error, exiting now.");
+            }
 
             gpio.Write(_bridgeEnablePin, PinValue.Low);
             gpio.ClosePin(_bridgeEnablePin);
