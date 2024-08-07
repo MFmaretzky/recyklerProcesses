595b1a3e2d222d497f2a78f97e24f4c5a440a88f
maretzky <marek.siuda711@outlook.com>
2024-08-07 11:36:17 +0200

    frontHatch object takes pins as arguments in Program.cs

--- Program.cs
+++ Program.cs
@@ -14,61 +14,29 @@ namespace console1
     {
         static void Main()
         {
-            FrontHatch frontHatch = new FrontHatch
-            {
-                UpperSensorPin = 16,
-                LowerSensorPin = 12,
-                MiddleSensorPin = 25
-            };
-            var gpio = new GpioController();
+            FrontHatch frontHatch = new(23, 16, 12, 25);
+            GpioController gpio = new();
             frontHatch.SenInit(gpio);
+
             while (true)
             {
-                var statuses = frontHatch.GetSenStat(gpio);
-                foreach (var status in statuses)
+                frontHatch.Process(gpio);
+                Console.Write("Do you wish to continue? (Y/n): ");
+                string? input = Console.ReadLine();
+
+                if (input != null)
                 {
-                    Console.WriteLine($"{!status}");
+                    input = input.Trim().ToLower();
                 }
-                Console.WriteLine("");
-                Thread.Sleep(2000);
+
+                if (input == "n")
+                {
+                    break;
+                }
             }
 
-            // while (true) {
-            //     frontHatch.Process(gpio);
-            //     Console.Write("Do you wish to continue? (Y/n): ");
-            //     string? input = Console.ReadLine(); // Read entire line which might return null
-
-            //     if (input != null) {
-            //         input = input.Trim().ToLower();
-            //     }
-
-            //     if (input == "n") {
-            //         break;
-            //     }
-            // }
-            // gpio.Dispose();
+            frontHatch.SenDeInit(gpio);
+            gpio.Dispose();
         }
     }
 }
-
-
-
-
-// FrontHatch frontHatch = new FrontHatch(
-//     upperSensorPin: (int)RPiPin.GPIO12,
-//     lowerSensorPin: (int)RPiPin.GPIO21,
-//     middleSensorPin: (int)RPiPin.GPIO14,
-//     bridgeEnablePin: (int)RPiPin.GPIO18,
-//     cwPolarisationPin: (int)RPiPin.GPIO24,
-//     ccwPolarisationPin: (int)RPiPin.GPIO23
-// );
-
-// frontHatch.SensorsInit(gpio);
-// Console.WriteLine("Wpisz 'o' by otworzyć przegrodę lub 'c' by zamknąć.");
-// Console.Write("Twoj wybor: ");
-// int i = 0;
-// while (i < 10) {
-//     frontHatch.Process(gpio);
-//     frontHatch.GetUpperStatus(gpio);
-//     i++;
-// }
